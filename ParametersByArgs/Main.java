6b5569f64d6e74e2cf6fdf4d9ae1dd8005820a5e
Ryan <ryaninternet62@gmail.com>
2020-04-20 15:18:08 -0300

    Passing parameters through the CMD to the Array, args of the main method, and their printing.

---
+++ ParametersByArgs/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+		
+		for (int i=0; i<args.length; i++) {
+			System.out.println("You wrote (by cmd): "+args[i]);
+		}
+		System.out.print("Exiting...");
+    }  
+}
+
+//java Main "My name is Ryan" "What's up?"
+//java Main Number1 Number2 3 4 5677
