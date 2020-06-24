9b58730f5bba4e869cf024195c469944c2e5f6e9
Ryan <ryaninternet62@gmail.com>
2020-06-24 14:57:13 -0300

    Situação de controle no seu Cadastro

---
+++ reservaHotelaria/src/Reserva.java
@@ -0,0 +1,36 @@
+public class Reserva {
+    private String data, hora, periodo;
+    private int numQuartos;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public int getNumQuartos() {
+        return numQuartos;
+    }
+
+    public void setNumQuartos(int numQuartos) {
+        this.numQuartos = numQuartos;
+    }  
+}
