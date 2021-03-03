54336af6198293b3e86f9de7b0f183ecbea4dcae
RyanCasf <ryaninternet62@gmail.com>
2021-03-03 04:16:31 -0300

    Study aplication about Scanner logic

--- scanInner/main.java
+++ scanInner/main.java
@@ -5,11 +5,17 @@ public class main{
 	
 	Scanner inner = new Scanner(System.in);
 	
-	System.out.println("\nEnter with your name:");
+	System.out.println("Enter a number: ");
+	//int index = Integer.parseInt(inner.nextLine());
+	int index = inner.nextInt();
+
+	// inner.nextLine();
+
+	System.out.println("\nEnter your name:");
 	String name = inner.nextLine();
 	
-	System.out.println("\nType your last name:");
-	String lastName = inner.nextLine();
+	System.out.println("\nEnter your last name:");
+	String lastName = inner.next();
 
 	System.out.print("\n Full name: " +name +" " +lastName +"\n");
     }
