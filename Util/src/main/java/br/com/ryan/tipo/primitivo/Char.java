576aaa247a34b2c835b90ffc8d7ea934bba35553
RyanCasf <ryaninternet62@gmail.com>
2021-10-02 15:40:58 -0300

    Testes com Char e Byte.

---
+++ Util/src/main/java/br/com/ryan/tipo/primitivo/Char.java
@@ -0,0 +1,19 @@
+package br.com.ryan.tipo.primitivo;
+
+public class Char 
+{
+	public static void main(String[] args) 
+	{
+		byte estouro = 127;
+		System.out.println(estouro++);
+		
+		char letra = 65;
+		System.out.println(letra);
+		
+		String daleTxt = "Dale";
+		for (char a : daleTxt.toCharArray())
+		{
+			System.out.print(a);
+		}
+	}
+}
