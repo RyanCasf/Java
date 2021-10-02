daebd5fbd1dfbf62083c946bfc2af80a52294fc7
RyanCasf <ryaninternet62@gmail.com>
2021-10-02 15:46:19 -0300

    Adionei os variantes do post e pre Fixo;

--- Util/src/main/java/br/com/ryan/tipo/primitivo/Char.java
+++ Util/src/main/java/br/com/ryan/tipo/primitivo/Char.java
@@ -5,7 +5,9 @@ public class Char
 	public static void main(String[] args) 
 	{
 		byte estouro = 127;
-		System.out.println(estouro++);
+		System.out.println(estouro++);  // 127 / -128
+		System.out.println(++estouro);  // -127 inveter
+		System.out.println(++estouro); // -126
 		
 		char letra = 65;
 		System.out.println(letra);
