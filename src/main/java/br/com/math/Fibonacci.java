9ad5e1e27e5bd29a973e7214f7d0ba47f6a614fe
= <ryaninternet62@gmail.com>
2022-01-04 20:33:38 -0300

    Add a calc para Fibonacci.

---
+++ src/main/java/br/com/math/Fibonacci.java
@@ -0,0 +1,45 @@
+package br.com.math;
+
+import java.util.Scanner;
+
+public class Fibonacci 
+{
+	private static Scanner scanner = new Scanner(System.in);
+	
+	static long fibonacci(int n) {
+        return (n < 2) ? n : fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) 
+    {
+    	try
+    	{
+    		int tamanho = obterTamanho();
+        	
+            for (int i=0; i<tamanho; i++) {
+                System.out.println("["+i+"]: "+Fibonacci.fibonacci(i));
+            }
+    	}
+    	catch (Exception e) 
+    	{
+    		System.out.println(e.getMessage());
+		}
+    }
+
+	private static int obterTamanho() throws Exception {
+		System.out.println("Cálcula para sequência de Fibonacci!");
+    	
+		System.out.println("Entre com o tamanho inteiro da posição: ");
+    	try {
+    		int tamanho = scanner.nextInt();
+    		
+    		if (tamanho <= 0) {
+        		throw new IllegalArgumentException("Valor inválido, deve ser maior que 0!");
+        	}
+    		return tamanho;
+		} 
+    	catch (Exception e) {
+    		throw new Exception("Algo deu errao!");
+		}
+	}
+}
