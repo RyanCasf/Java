b6641c98e5cb15d4aa2a7b6412421b25d890e376
= <ryaninternet62@gmail.com>
2022-01-04 20:55:08 -0300

    Criação de arquivo by Java

---
+++ src/main/java/br/com/scan/Scan.java
@@ -0,0 +1,27 @@
+package br.com.scan;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Scan 
+{
+	public static void main(String[] args)  
+	{
+		Scanner sc = null;
+		try {
+			File file = new File("C:\\Users\\ryanc\\filename.txt");
+			
+			sc = new Scanner(file);
+			while (sc.hasNextLong()) {
+				long aLong = sc.nextLong();
+			}
+			
+			sc.close();
+		} 
+		catch (IOException e) 
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
