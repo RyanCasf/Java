385fe851b020d8a212f0a9b6cdc9094172dff244
= <ryaninternet62@gmail.com>
2022-01-15 21:51:11 -0300

    Format String do Integer. - REGEX

---
+++ src/main/java/br/com/regex/FormatInt.java
@@ -0,0 +1,28 @@
+package br.com.regex;
+
+import java.util.Random;
+
+public class FormatInt 
+{
+	private static Random random = new Random();
+	private static final int SIZE = 3;
+	
+	public static void main(String[] args) 
+    {
+		String[] strings = new String[SIZE];
+		int[] inteiros = new int[SIZE];
+		
+        System.out.println("================================");
+        
+        for(int i=0; i<SIZE; i++) {
+        	strings[i] = Integer.toString(random.nextInt(100));
+            inteiros[i] = random.nextInt(100);
+        }
+        
+        for(int i=0; i<SIZE; i++) {
+        	System.out.println(strings[i]+(String.format("%-"+(15-strings[i].length())+"s", ""))+(String.format("%03d", inteiros[i])));
+        }
+        
+        System.out.println("================================");
+    }
+}
