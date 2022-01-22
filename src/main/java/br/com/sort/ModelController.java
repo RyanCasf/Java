ce11dfd3aca5f9837ff4ad60d9f17ddf0eda8852
= <ryaninternet62@gmail.com>
2022-01-22 04:33:52 -0300

    Adicionei a comparação do Comparador

---
+++ src/main/java/br/com/sort/ModelController.java
@@ -0,0 +1,25 @@
+package br.com.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ModelController 
+{
+	private static final Random random = new Random();
+	
+	public static List<Model> fetchModels(int quantidade) {
+		List<Model> temp = new ArrayList<>();
+		
+		for (int i=0; i<quantidade; i++) {
+			temp.add(new Model(Long.valueOf(i), ""+i, ""+i));
+			int tamanhoParaLista = Math.abs(random.nextInt(10) + 1);
+			
+			for (int j=0; j<tamanhoParaLista; j++) {
+				temp.get(i).getModelsFilhos().add(new Model(Long.valueOf(j), ""+j, ""+j));
+			}
+		}
+		
+		return temp;
+	}
+}
