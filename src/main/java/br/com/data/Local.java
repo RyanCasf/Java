cfaddd421ec1f02f2ed4d544a9b8fca67c8f1493
= <ryaninternet62@gmail.com>
2022-06-16 16:29:55 -0300

    Adicionei as considerações sobre o java.Date,

--- src/main/java/br/com/data/Local.java
+++ src/main/java/br/com/data/Local.java
@@ -1,9 +1,21 @@
 package br.com.data;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 public class Local {
+	
 	public static LocalDate getDataAmanha() {
 		return LocalDate.now().plusDays(1);
+	}
+	
+	public static Date getDate(int day, int month, int year) {
+		Date temp = new Date();
+		
+		temp.setDate(day);
+		temp.setMonth((month - 1));
+		temp.setYear((year - 1900));
+		
+		return temp;
 	}
 }
--- src/test/java/br/com/data/LocalTeste.java
+++ src/test/java/br/com/data/LocalTeste.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 public class LocalTeste {
 	
@@ -13,5 +14,14 @@ public class LocalTeste {
         LocalDate amanhaTeste = Local.getDataAmanha();
 
         Assert.assertEquals(amanha, amanhaTeste);
+    }
+    
+    @Test
+    public void textToDate() {
+    	Date date = Local.getDate(16, 6, 2022);
+    	
+    	Assert.assertEquals(16, date.getDate());
+    	Assert.assertEquals((6 - 1), date.getMonth());
+    	Assert.assertEquals((2022 - 1900), date.getYear());
     }
 }
