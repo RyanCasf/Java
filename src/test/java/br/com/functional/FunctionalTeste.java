eba08e5191dde19de928d170482e98827e7f76c4
= <ryaninternet62@gmail.com>
2022-06-16 16:48:06 -0300

    Segreguei a execução para o Teste.

--- src/test/java/br/com/functional/FunctionalTeste.java
+++ src/test/java/br/com/functional/FunctionalTeste.java
@@ -2,20 +2,23 @@ package br.com.functional;
 
 import java.math.BigDecimal;
 
-public class Lambda {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FunctionalTeste {
 	private static final int PARAM = 0;
 	
-	public static void main(String[] args) {
+	@Test
+	public void lambdas() {
 		int resultDirectStatic = increment(PARAM);
 		int resultByFunction = Functions.incrementByFunction.apply(PARAM);
 
-		System.out.println("01. : " + resultDirectStatic + "\n02. : " + resultByFunction);
+		Assert.assertEquals(1, resultDirectStatic);
+		Assert.assertEquals(1, resultByFunction);
 		
 		BigDecimal multiply = Functions.multiplyByFunction.apply(new BigDecimal[] { BigDecimal.TEN, BigDecimal.TEN });
-		System.out.println("\n10 * 10 = " + multiply);
-		
 		multiply = Functions.multiplyTwoByFunction.apply(BigDecimal.TEN, BigDecimal.TEN);
-		System.out.println("\n10 * 10 = " + multiply);
+		Assert.assertEquals((BigDecimal.valueOf(100)), multiply);
 	}
 
 	static int increment(int number) {
