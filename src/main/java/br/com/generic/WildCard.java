17de613482fe3590fd1063284ba98dcd5799473e
= <ryaninternet62@gmail.com>
2023-06-15 05:25:32 -0300

    Adicionei o wild card para list.

---
+++ src/main/java/br/com/generic/WildCard.java
@@ -0,0 +1,44 @@
+package br.com.generic;
+
+import java.util.List;
+
+abstract class Animal {
+	public abstract void consultar();
+}
+
+class Cachorro extends Animal {
+	
+	@Override
+	public void consultar() {
+		
+	}
+}
+
+class Gato extends Animal {
+	
+	@Override
+	public void consultar() {
+		
+	}
+}
+
+public class WildCard {
+	
+	public void printConsultar(Animal[] animais) {
+		if (animais == null || animais.length == 0)
+			return;
+		
+		for (Animal animal : animais) {
+			animal.consultar();
+		}
+	}
+	
+	public void printConsultar(List<? extends Animal> animais) {
+		if (animais == null || animais.isEmpty())
+			return;
+		
+		for (Animal animal : animais) {
+			animal.consultar();
+		}
+	}
+}
---
+++ src/test/java/br/com/generic/WildCardTeste.java
@@ -0,0 +1,72 @@
+package br.com.generic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@DisplayName("WildCard")
+class WildCardTeste {
+	
+	private WildCard wildCard;
+	
+	@BeforeEach
+	void novo() {
+		wildCard = new WildCard();
+	}
+	
+	@Test @DisplayName("Printar consultar com animais vazio.")
+	void printConsultarComAnimaisVazio() {
+		Animal[] animais = new Animal[0];
+		assertDoesNotThrow(() -> wildCard.printConsultar(animais));
+	}
+	
+	@Test @DisplayName("Printar consultar animais com sucesso (cachorros).")
+	void printConsultarComSucessoCachorros() {
+		Cachorro[] cachorros = {new Cachorro(), new Cachorro()};
+		wildCard.printConsultar(cachorros);
+	}
+	
+	@Test @DisplayName("Printar consultar animais com sucesso (gatos).")
+	void printConsultarComSucessoGatos() {
+		Gato[] gatos = {new Gato(), new Gato()};
+		wildCard.printConsultar(gatos);
+	}
+	
+	@Test @DisplayName("Printar consultar animais com sucesso (animais).")
+	void printConsultarComSucessoAnimais() {
+		Animal[] animais = {new Cachorro(), new Gato()};
+		wildCard.printConsultar(animais);
+	}
+	
+	@Test @DisplayName("Printar consultar list com animais vazio.")
+	void printConsultarListComAnimaisVazio() {
+		assertDoesNotThrow(() -> wildCard.printConsultar(new ArrayList<>()));
+	}
+	
+	@Test @DisplayName("Printar consultar list animais com sucesso (cachorros).")
+	void printConsultarListComSucessoCachorros() {
+		List<Cachorro> cachorros = Arrays.asList(new Cachorro(), new Cachorro());
+		wildCard.printConsultar(cachorros);
+	}
+	
+	@Test @DisplayName("Printar consultar list animais com sucesso (gatos).")
+	void printConsultarListComSucessoGatos() {
+		List<Gato> gatos = Arrays.asList(new Gato(), new Gato());
+		wildCard.printConsultar(gatos);
+	}
+	
+	@Test @DisplayName("Printar consultar list animais com sucesso (animais).")
+	void printConsultarListComSucessoAnimais() {
+		List<Animal> animais = Arrays.asList(new Cachorro(), new Gato());
+		wildCard.printConsultar(animais);
+	}
+}
