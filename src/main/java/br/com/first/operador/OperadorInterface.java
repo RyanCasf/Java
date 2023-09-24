40eb62497bfe35f0a8b5944d36eabd940cdc7c6a
= <ryaninternet62@gmail.com>
2023-09-24 20:52:39 -0300

    Removendo simplificações de conteúdo.

--- src/main/java/br/com/first/operador/OperadorInterface.java
+++
@@ -1,8 +0,0 @@
-package br.com.first.operador;
-
-public interface OperadorInterface {
-	
-	public double somar(double... numeros) throws NullPointerException;
-	public double subtrair(double numero1, double numero2);
-	public double multiplicar(double... numeros) throws NullPointerException;
-}
